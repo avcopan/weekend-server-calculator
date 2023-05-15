33c479b9575fd73438bb6a0f913ba0603ab82e4e
Andreas Copan <avcopan@gmail.com>
2023-05-15 10:17:18 -0400

    Serve static webpage

--- server/server.js
+++ server/server.js
@@ -3,10 +3,10 @@ const express = require("express");
 const app = express();
 const PORT = 8000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on: ${PORT}`);
-});
+// start server spin-up
+app.use(express.static("server/public"));
 
-app.get("/testing", (req, res) => {
-  res.send("TESTING TESTING");
+app.listen(PORT, () => {
+  console.log("Listening on", PORT);
 });
+// end server spin-up
