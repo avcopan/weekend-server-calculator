2d7b19bad5957d870138f4d0273f64ccd536c4fe
Andreas Copan <avcopan@gmail.com>
2023-05-15 14:36:11 -0400

    Implements server-side calculation

--- server/server.js
+++ server/server.js
@@ -2,12 +2,6 @@
 const express = require("express");
 const app = express();
 const PORT = 8000;
-const operationSymbol = {
-  add: "+",
-  subtract: "−",
-  multiply: "×",
-  divide: "÷",
-};
 const operationFunction = {
   "+": (num1, num2) => num1 + num2,
   "−": (num1, num2) => num1 - num2,
@@ -25,17 +19,18 @@ let answer;
  * @param {*} operations - The arithmetic operations to evaluate.
  * @returns The a new calculation array, after evaluating the requested operations.
  */
-const multiplyAndDivide = (calcArray) => {
-  const isMultiplyOrDivide = (element) =>
-    element == operationSymbol.multiply || element == operationSymbol.divide;
-  while (calcArray.includes(operationSymbol.multiply) ||
-         calcArray.includes(operationSymbol.divide)) {
-    let opIndex = calcArray.findIndex(isMultiplyOrDivide, calcArray);
+const evaluateOperations = (calcArray, operations) => {
+  const isRequestedOp = (item) => operations.includes(item);
+  while (calcArray.some(isRequestedOp)) {
+    let opIndex = calcArray.findIndex(isRequestedOp, calcArray);
+    // Get the operation and the numbers involved
     let op = calcArray[opIndex];
-    let num1 = calcArray[opIndex - 1];
-    let num2 = calcArray[opIndex + 1];
+    let num1 = Number(calcArray[opIndex - 1]);
+    let num2 = Number(calcArray[opIndex + 1]);
+    // Evaluate to get the result
     let result = operationFunction[op](num1, num2);
     console.log(`${opIndex}: ${num1} ${op} ${num2} = ${result}`);
+    // Update the calculation, replacing this operation with the result
     calcArray.splice(opIndex - 1, 3, result);
     console.log("Sliced calcArray:", calcArray);
   }
@@ -49,29 +44,36 @@ const multiplyAndDivide = (calcArray) => {
  * @returns {number} The final result of the calculation.
  */
 const calculateAnswer = (calcArray) => {
-  calcArray = multiplyAndDivide(calcArray);
-  const answer = Number(calcArray[0]);
-  console.log("For with semicolons:");
-  for (let i = 1; i < calcArray.length; i += 2) {
-    console.log(calcArray[i], calcArray[i + 1]);
-  }
-  return answer;
+  // Evaluate multiplication and division
+  calcArray = evaluateOperations(calcArray, ["×", "÷"]);
+  // Evaluate addition and subtraction
+  calcArray = evaluateOperations(calcArray, ["+", "−"]);
+  // We should now only have one element remaining
+  console.assert(
+    calcArray.length == 1,
+    "After evaluating all operations, calcArray should consist of a",
+    "single element, but it doesn't",
+    calcArray
+  );
+  return calcArray[0];
 };
 
 // server
 app.use(express.static("server/public"));
 app.use(express.json());
 
+app.listen(PORT, () => {
+  console.log("Listening on", PORT);
+});
+
 app.post("/calculation", (req, res) => {
   console.log("Received:", req.body);
   answer = calculateAnswer(req.body);
+  console.log("Calculated answer:", answer);
   res.sendStatus(201);
 });
 
 app.get("/calculation", (req, res) => {
-  res.send(answer);
-});
-
-app.listen(PORT, () => {
-  console.log("Listening on", PORT);
+  console.log("Attempting to send answer:", answer);
+  res.send(String(answer));
 });
